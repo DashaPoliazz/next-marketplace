1c778436386a8fddeec7523bb65bb10b6706170f
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-12-26 17:09:47 +0200

    component HTag has been implemented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Home from ".";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Marketplae</title>
+        <link rel="icon" href="./favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <Home />
+    </>
+  );
 }
